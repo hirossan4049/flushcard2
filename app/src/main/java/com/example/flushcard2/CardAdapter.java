53d389c9a7aa91d3861323d0b112264081b79d72
hirossan4049 <haruto405329@gmail.com>
2019-06-05 05:01:29 +0900

    flushcard2_ok

--- app/src/main/java/com/example/flushcard2/CardAdapter.java
+++ app/src/main/java/com/example/flushcard2/CardAdapter.java
@@ -1,12 +1,14 @@
 package com.example.flushcard2;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -34,6 +36,35 @@ public class CardAdapter extends ArrayAdapter<Card> {
     @Override
     public View getView(final  int position, View convertView , ViewGroup parent){
         final ViewHolder viewHolder;
+
+        if (convertView == null){
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.card,null);
+            viewHolder = new ViewHolder(convertView);
+            convertView.setTag(viewHolder);
+        }else{
+            viewHolder = (ViewHolder)convertView.getTag();
+        }
+        final Card item = getItem(position);
+
+        if (item != null){
+            //setdata
+            viewHolder.iconImageView.setBackgroundResource(item.imageId);
+            viewHolder.titleTextView.setText(item.title);
+            viewHolder.contentTextView.setText(item.content);
+            viewHolder.likeButton.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v){
+                    item.likeNum++;
+                    viewHolder.likeTextView.setText(item.likeNum + "Likes");
+                }
+            });
+            viewHolder.iconImageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(getContext(),item.meaning, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
 
         return convertView;
     }
--- app/src/main/java/com/example/flushcard2/MainActivity.java
+++ app/src/main/java/com/example/flushcard2/MainActivity.java
@@ -3,8 +3,16 @@ package com.example.flushcard2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.LinearLayout;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    List<Card> mCards;
+    CardAdapter mCardAdapter;
+    ListView mListView;
+
 
 
 
@@ -13,7 +21,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mListView = (ListView)findViewById(R.id.listView);
+        mCards = new ArrayList<Card>();
 
+        mCards.add(new Card(R.drawable.icon1,getString(R.string.walk_title),
+                getString(R.string.walk_content),getString(R.string.walk_meaning)));
+        mCards.add(new Card(R.drawable.icon2,getString(R.string.jump_title),
+                getString(R.string.jump_content),getString(R.string.jump_meaning)));
+
+        mCardAdapter = new CardAdapter(this,R.layout.card,mCards);
+        mListView.setAdapter(mCardAdapter);
 
 
 
